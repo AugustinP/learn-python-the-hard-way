0f158adf189de5188eacf2851deb7d31e7979a0f
Imogen Wentworth <imogen.wentworth@gmail.com>
2013-05-05 19:56:55 +0100

    Added exercise

---
+++ ex3.py
@@ -0,0 +1,33 @@
+#Printing intent phrase
+print "I will now count my chickens:"
+
+#Calculating number of hens and roosters
+#Using floating point numbers where necessary to make results more accurate
+print "Hens", 25 + 30 / 6
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+
+#Printing intent phrase
+print "Now I will count the eggs:"
+
+#Calculating number of eggs
+#Using floating point numbers to make results more accurate
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+#Asking a question
+print "Is it true that 3 + 2 < 5 - 7?"
+
+#Math to answer the question
+print 3 + 2 < 5 - 7
+
+#Asking and answering more calculation questions
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+#More questions
+print "Oh, that's why it's False."
+print "How about some more."
+
+#More questions and calculations
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
